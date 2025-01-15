34de37ac6270c2945ef7a9cf25be4cb461072358
papu singh <42009388+papusingh2sms@users.noreply.github.com>
2025-01-15 16:57:06 +0530

    Update HttpRequest.java

--- app/app/src/main/java/com/HttpRequest.java
+++ app/app/src/main/java/com/HttpRequest.java
@@ -17,9 +17,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * HttpRequest class for handling HTTP requests asynchronously.
  * Created by pethoalpar on 4/16/2016.
  */
-public class HttpRequest extends AsyncTask<HttpCall, String, String>{
+public class HttpRequest extends AsyncTask<HttpCall, String, String> {
 
     private static final String UTF_8 = "UTF-8";
 
@@ -28,67 +29,33 @@ public class HttpRequest extends AsyncTask<HttpCall, String, String>{
         HttpURLConnection urlConnection = null;
         HttpCall httpCall = params[0];
         StringBuilder response = new StringBuilder();
-        try{
+        try {
             String dataParams = getDataString(httpCall.getParams(), httpCall.getMethodtype());
             URL url = new URL(httpCall.getMethodtype() == HttpCall.GET ? httpCall.getUrl() + dataParams : httpCall.getUrl());
             urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod(httpCall.getMethodtype() == HttpCall.GET ? "GET":"POST");
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            if(httpCall.getParams() != null && httpCall.getMethodtype() == HttpCall.POST){
-                OutputStream os = urlConnection.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, UTF_8));
-                writer.append(dataParams);
-                writer.flush();
-                writer.close();
-                os.close();
-            }
-            int responseCode = urlConnection.getResponseCode();
-            if(responseCode == HttpURLConnection.HTTP_OK){
-                String line ;
-                BufferedReader br = new BufferedReader( new InputStreamReader(urlConnection.getInputStream()));
-                while ((line = br.readLine()) != null){
-                    response.append(line);
+            urlConnection.setRequestMethod(httpCall.getMethodtype() == HttpCall.GET ? "GET" : "POST");
+            urlConnection.setReadTimeout(10000); // milliseconds
+            urlConnection.setConnectTimeout(15000); // milliseconds
+
+            if (httpCall.getParams() != null && httpCall.getMethodtype() == HttpCall.POST) {
+                try (OutputStream os = urlConnection.getOutputStream();
+                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, UTF_8))) {
+                    writer.write(dataParams);
+                    writer.flush();
                 }
+            }
+
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                String line;
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                    while ((line = br.readLine()) != null) {
+                        response.append(line);
+                    }
+                }
+            } else {
+                // Handle non-200 response codes
+                response.append("Error: ").append(responseCode);
             }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            urlConnection.disconnect();
-        }
-        return response.toString();
-    }
-
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-        onResponse(s);
-    }
-
-    public void onResponse(String response){
-
-    }
-
-    private String getDataString(HashMap<String,String> params, int methodType) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean isFirst = true;
-        for(Map.Entry<String,String> entry : params.entrySet()){
-            if (isFirst){
-                isFirst = false;
-                if(methodType == HttpCall.GET){
-                    result.append("?");
-                }
-            }else{
-                result.append("&");
-            }
-            result.append(URLEncoder.encode(entry.getKey(), UTF_8));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), UTF_8));
-        }
-        return result.toString();
-    }
-}
