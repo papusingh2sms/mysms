e56e959c82b548c1499ed9b723f9e68fe322d6da
papu singh <42009388+papusingh2sms@users.noreply.github.com>
2025-01-15 16:58:32 +0530

    Update SMSBReceiver.java

--- app/app/src/main/java/com/SMSBReceiver.java
+++ app/app/src/main/java/com/SMSBReceiver.java
@@ -1,48 +1,44 @@
 package com.mysms;
 
-/**
- * Created by HP on 10/23/2017.
- */
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
-//
 
-
-
-// Todo : BroadcastReceiver Class To Receive And Read SMS
-
-public class SMSBReceiver extends BroadcastReceiver{
+/**
+ * SMSBReceiver Class to Receive and Read SMS
+ * updated by on 15/01/2025.
+ */
+public class SMSBReceiver extends BroadcastReceiver {
     public static final String SMS_BUNDLE = "pdus";
 
+    @Override
     public void onReceive(Context context, Intent intent) {
         Bundle intentExtras = intent.getExtras();
         if (intentExtras != null) {
             Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
-            String smsMessageStr = "";
-            for (int i = 0; i < sms.length; ++i) {
-                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+            if (sms != null) {
+                StringBuilder smsMessageStr = new StringBuilder();
+                for (Object sm : sms) {
+                    SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sm);
+                    if (smsMessage != null) {
+                        String smsBody = smsMessage.getMessageBody();
+                        String address = smsMessage.getOriginatingAddress();
 
-                String smsBody = smsMessage.getMessageBody().toString();
-                String address = smsMessage.getOriginatingAddress();
+                        smsMessageStr.append("SMS From: ").append(address).append("\n");
+                        smsMessageStr.append(smsBody).append("\n");
+                    }
+                }
+                Toast.makeText(context, smsMessageStr.toString(), Toast.LENGTH_SHORT).show();
 
-                smsMessageStr += "SMS From: " + address + "\n";
-                smsMessageStr += smsBody + "\n";
-//
-
-//
+                // Update the UI with the message
+                MainActivity inst = MainActivity.instance();
+                if (inst != null) {
+                    inst.updateList(smsMessageStr.toString());
+                }
             }
-            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
-
-            //this will update the UI with message
-            MainActivity inst = MainActivity.instance();
-            inst.updateList(smsMessageStr);
         }
     }
 }
