0c67ad44cc4c611c4a61b3183f570d6f0b4cb654
papu singh <42009388+papusingh2sms@users.noreply.github.com>
2025-01-15 18:45:20 +0530

    Update mysms.sh

--- mysms.sh
+++ mysms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MySMS v1.0
-# Author: @thelinuxchoice (You don't become a coder by just changing the credits)
-# Github: https://github.com/thelinuxchoice/mysms
+# Author: @papusingh2sms (You don't become a coder by just changing the credits)
+# Github: https://github.com/papusingh2sms/mysms
 
 host="159.89.214.31" #Serveo.net
 
